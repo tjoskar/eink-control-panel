2b2f4054993e25d77b367bfc4870a29ea041fc0c
tjoskar <hello@tjoskar.com>
2025-05-23 19:38:24 +0200

    f

--- electricity_price.py
+++ electricity_price.py
@@ -60,9 +60,16 @@ def draw_price_chart(draw, pos, width, height):
         y = pos[1] + chart_height - (p - min_price) * y_scaling
         points.append((x, y))
 
-    # Draw the line connecting all points
+    # Draw step chart with vertical lines between points
     if len(points) > 1:
-        draw.line(points, fill=colors["black"], width=2)
+        for i in range(len(points) - 1):
+            # Draw horizontal line from current point to below the next point
+            draw.line([(points[i][0], points[i][1]), (points[i+1][0], points[i][1])],
+                      fill=colors["black"], width=2)
+
+            # Draw vertical line from below the next point to the next point
+            draw.line([(points[i+1][0], points[i][1]), (points[i+1][0], points[i+1][1])],
+                      fill=colors["black"], width=2)
 
     # Only draw dot at highlight_index
     if 0 <= highlight_index < len(points):
